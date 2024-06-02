c2e4c2d63ffad1f1ff267205f2d4753765734fab
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-02 22:16:54 +0530

    added routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { signIn } from "../zod/Signin";
+import { register } from "../zod/Register";
 
 const app = express();
 const PORT = 3000;
@@ -6,4 +8,8 @@ app.use(express.json());
 
 app.listen(()=>{
     console.log(`app listening on ${PORT}`);
+})
+
+app.post('/register',(req,res)=>{
+    const body = req.body.json();
 })
