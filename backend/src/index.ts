0a18d7b4f436bd73e4726b5e6850585392b68acf
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-03 21:37:07 +0530

    updating index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { signIn } from "../zod/Signin";
-import { register } from "../zod/Register";
+import { signIn } from "./zod/Signin";
+import { register } from "./zod/Register";
 
 const app = express();
 const PORT = 3000;
@@ -10,8 +10,14 @@ app.listen(()=>{
     console.log(`app listening on ${PORT}`);
 })
 
-app.post('/register',(req,res)=>{
-    const body = req.body.json();
+app.post('/register',async(req,res)=>{
+    const body = await req.body.json();
+    const {success} = signIn.safeParse(body);
+    if(!success){
+        res.status(411).send({
+            message : "Please send correct inputs"
+        })
+    } 
 })
 app.post('/signin',(req,res)=>{
     const body = req.body.json();
