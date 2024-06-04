f27410e5b2986072909522620504e28a16f3ae7a
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-04 22:01:38 +0530

    Creating new user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,34 @@
 import express from "express";
 import { signIn } from "./zod/Signin";
 import { register } from "./zod/Register";
-
+import { PrismaClient, Prisma } from '@prisma/client'
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-app.listen(()=>{
-    console.log(`app listening on ${PORT}`);
-})
+const prisma = new PrismaClient();
+
 
 app.post('/register',async(req,res)=>{
-    const body = await req.body.json();
-    const {success} = signIn.safeParse(body);
+    const {success} = signIn.safeParse(req.body);
     if(!success){
-        res.status(411).send({
+        res.status(411).json({
             message : "Please send correct inputs"
         })
-    } 
+    }
+    //creating a new User
+    const User = await prisma.user.create({
+        data: {
+          username : req.body.username,
+          password: req.body.password,
+          name: req.body.name,
+        },
+      });
 })
 app.post('/signin',(req,res)=>{
     const body = req.body.json();
+})
+
+app.listen(()=>{
+    console.log(`app listening on ${PORT}`);
 })
