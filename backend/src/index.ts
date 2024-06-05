1f41eba2edba0148f157ad1222da1ee309c30147
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-06-05 22:32:58 +0530

    Updating index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,59 @@
 import express from "express";
 import { signIn } from "./zod/Signin";
 import { register } from "./zod/Register";
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from "@prisma/client";
 const app = express();
 const PORT = 3000;
+const jwt = require("jsonwebtoken");
 app.use(express.json());
 
 const prisma = new PrismaClient();
 
+app.post("/register", async (req, res) => {
+  // const {username,password,name} = req.body.json();
+  // const {success} = register.safeParse(body);
+  // if(!success){
+  //     res.status(411).json({
+  //         message : "Please send correct inputs"
+  //     })
+  // }
+  //creating a new User
+//   const parsedBody = register.parse(req.body);
+//   const { username, password, name } = parsedBody;
 
-app.post('/register',async(req,res)=>{
-    const {success} = signIn.safeParse(req.body);
-    if(!success){
-        res.status(411).json({
-            message : "Please send correct inputs"
-        })
-    }
-    //creating a new User
-    const User = await prisma.user.create({
-        data: {
-          username : req.body.username,
-          password: req.body.password,
-          name: req.body.name,
-        },
-      });
-})
-app.post('/signin',(req,res)=>{
-    const body = req.body.json();
-})
+//   const user = await prisma.user.create({
+//     data: {
+//       username,
+//       password,
+//       name,
+//     },
+//   });
 
-app.listen(()=>{
-    console.log(`app listening on ${PORT}`);
-})
+//   //creating jwt
+//   const token = await jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+//   return res.json({
+//     message: "User created successfully",
+//     jwt: token,
+//   });
+const users = [];
+const parsedBody = register.parse(req.body);
+const { username, password, name } = parsedBody;
+
+const newUser = {
+    id: users.length + 1,
+    username,
+    password,
+    name
+  };
+  users.push(newUser);
+  return res.json({
+    message: "User created successfully",
+  });
+});
+app.post("/signin", (req, res) => {
+  const body = req.body.json();
+});
+
+app.listen(() => {
+  console.log(`app listening on ${PORT}`);
+});
